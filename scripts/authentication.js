05de0ed9e92fb0b0dc2a2342dbfcc96eefeda3d8
shye <113232835+justshye@users.noreply.github.com>
2022-11-10 11:28:28 -0800

    added default values to firestore

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -20,12 +20,21 @@ var uiConfig = {
         db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
           name: user.displayName,                    //"users" collection
           email: user.email,                         //with authenticated user's ID (user.uid)
+          totalTime: 0,
+          totalSessions: 0,
+          averageDuration: 0,
           //country: "Canada",                      //optional default profile info      
           //school: "BCIT"                          //optional default profile info
         }).then(function () {
           console.log("New user added to firestore");
           window.location.assign("main.html");       //re-direct to main.html after signup
+        }
+        db.collection("users").doc(user.uid).collection("pet").doc().set({
+          totalExp: 0,
+          level: 1,
+          height: 0.105,
         })
+        )
           .catch(function (error) {
             console.log("Error adding new user: " + error);
           });
