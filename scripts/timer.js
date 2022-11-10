b862bd5a6215edbef2a17a0fa5b42eda94e240a8
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2022-11-10 11:52:05 -0800

    Update Timer Files

--- scripts/timer.js
+++ scripts/timer.js
@@ -76,38 +76,63 @@ function startTimer() {
     countdown(duration);
 }
 function countdown(duration) {
-    console.log("in countdownn " + duration);
     // Set the date we're counting down to
     // var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
-    var newDate = new Date();
+    var newDate = new Date(); //startime
     newDate.setTime(newDate.getTime() + duration*60*1000);
     var countDownDate = newDate.getTime();
 
     // Update the count down every 1 second
-    var x = setInterval(function () {
+    var x = setInterval(function() {
 
         // Get today's date and time
         var now = new Date().getTime();
 
         // Find the distance between now and the count down date
         var distance = countDownDate - now;
-        console.log("dist is: " + distance);
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const angle = (countDownDate / distance) * 360;
 
-        // Display the result in the element with id="demo"
-        document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-            minutes + "m " + seconds + "s ";
+        // progress indicator
+        if (angle > 180) {
+            semicircles[2].style.display ='none';
+            semicircles[0].style.transform = 'rotate(180deg)';
+            semicircles[1].style.transform = `rotate(${angle}deg)`;
+        } else {
+            semicircles[2].style.display ='block';
+            semicircles[0].style.transform = `rotate(${angle}deg)`;
+            semicircles[1].style.transform = `rotate(${angle}deg)`;
+        }
+        
+        // Time calculations for hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+
+        // Display the result
+        timer.innerHTML = `
+        <div>${hours}</div>
+        <div class="colon">:</div>
+        <div>${minutes}</div>
+        <div class="colon">:</div>
+        <div>${seconds}</div>`;
 
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("demo").innerHTML = "EXPIRED";
+            semicircles[0].style.display ='none';
+            semicircles[1].style.display ='none';
+            semicircles[2].style.display ='none';
+
+            timer.innerHTML = `
+            <div>00</div>
+            <div class="colon">:</div>
+            <div>00</div>
+            <div class="colon">:</div>
+            <div>00</div>
+            `;
+            timer.style.color = "lightgray";
         }
     }, 1000);
 }
