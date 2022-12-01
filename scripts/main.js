24f56d09d7c1f3d611a8a1993c9e5b8b18646e08
shye <113232835+justshye@users.noreply.github.com>
2022-11-30 17:32:01 -0800

    uh here's a small commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,3 @@
-// function insertLevel() {
-//     firebase.auth().onAuthStateChanged(user => {
-//         // Check if a user is signed in:
-//         if (user) {
-//             // Do something for the currently logged-in user here: 
-//             console.log(user.uid); //print the uid in the browser console
-//             console.log(user.displayName);  //print the user name in the browser console
-//             user_Name = user.displayName;
-
-//             //method #1:  insert with html only
-//             //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-//             //method #2:  insert using jquery
-//             $("#level-goes-here").text(user_Name); //using jquery
-
-//         } else {
-//             // No user is signed in.
-//         }
-//     });
-// }
-// insertName(); //run the function
-
 function populateInfo() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
